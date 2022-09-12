875340e20c99ebce36076499a2a42bca54c8cc01
hxy <727817849@qq.com>
2022-09-12 15:47:27 +0800

    引入iconfontsympol字体

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,12 @@ export default {
 </script>
 
 <style>
+/* 全局设置iconfont字体图标样式 */
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,23 +5,26 @@
       test
       <van-button type="primary">主要按钮</van-button>
     </div>
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-huasheng"></use>
+    </svg>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-.hello{
+.hello {
   font-size: 20px;
-  .test{
+  .test {
     color: red;
   }
 }
